c756aa17d3c8e8de6672e4ed123a10079384405c
Gustavo Oliveira <gu.machado.oliveira@gmail.com>
2025-03-28 10:53:55 -0300

    First route

--- src/pedestal_api/core.clj
+++ src/pedestal_api/core.clj
@@ -1,6 +1,19 @@
-(ns pedestal-api.core)
+(ns pedestal-api.core
+  (:require [io.pedestal.http.route :as route]
+            [io.pedestal.http :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn hello-fn [request]
+  {:status 200 :body "Hello World"})
+
+(def routes (route/expand-routes #{["/hello" ;;endpoint
+                                    :get ;;method
+                                    hello-fn ;;function that return from request
+                                    :route-name :hello-world]})) ;;every route must have a unique name
+
+(def service-map {::http/routes routes
+                  ::http/port 9999
+                  ::http/type :jetty ;;define the server, can be another
+                  ::http/join? false}) ;;prevent to block Clojure thread (?)
+
+(http/start (http/create-server service-map))
+(prn "Server running")
