848ca4950bafae1098900e9a5cd4734b4ff8707d
Gustavo Oliveira <gu.machado.oliveira@gmail.com>
2025-03-31 09:41:17 -0300

    Refact the server implements interceptor to inject store

--- src/pedestal_api/server.clj
+++ src/pedestal_api/server.clj
@@ -1,9 +1,8 @@
 (ns pedestal-api.server
   (:require [io.pedestal.http.route :as route]
             [io.pedestal.http :as http]
-            [io.pedestal.test :as test]))
-
-(def store (atom {}))
+            [io.pedestal.test :as test]
+            [pedestal-api.database :as database]))
 
 (def server (atom nil))
 
@@ -13,21 +12,29 @@
 (defn create-task [req]
   (let [uuid (java.util.UUID/randomUUID)
         task (-> req :query-params :task)
-        status (-> req :query-params :status)]
+        status (-> req :query-params :status)
+        store (:store req)]
     (swap! store assoc uuid {:id uuid :task task :status status})
     {:status 200 :body {:msg "Task created successful"
                         :task {:id uuid :task task :status status}}}))
 
-(defn get-tasks [_req]
-  {:status 200 :body @store})
+(defn get-tasks [req]
+  {:status 200 :body @(:store req)})
+
+(defn assoc-store [context]
+  (update context :request assoc :store database/store))
+
+(def db-interceptor
+  {:name :db-interceptor
+   :enter assoc-store})
 
 (def routes (route/expand-routes
               #{["/hello" ;;endpoint
                  :get ;;method
                  hello-fn ;;function that return from request
                  :route-name :hello-world] ;;every route must have a unique name
-                ["/task" :post create-task :route-name :create-task]
-                ["/tasks" :get get-tasks :route-name :get-tasks]}))
+                ["/task" :post [db-interceptor create-task] :route-name :create-task]
+                ["/tasks" :get [db-interceptor get-tasks] :route-name :get-tasks]}))
 
 (def service-map {::http/routes routes
                   ::http/port 9999
@@ -36,6 +43,15 @@
 
 (defn start-server []
   (reset! server (http/start(http/create-server service-map))))
+
+(defn stop-server []
+  (http/stop @server))
+
+(defn restart-server []
+  (stop-server)
+  (start-server))
+
+(comment (restart-server))
 
 (defn test-request [verb url]
   (test/response-for (::http/service-fn @server) verb url))
